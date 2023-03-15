1fc38194d94b55cdc203da368b96083799b60a28
Charly Correa <chuck.scrumdev@gmail.com>
2023-03-14 22:41:09 -0500

    Dinamic environment config and validation using configType and Joi requierments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-
-import { environment } from './config/environments';
-import config from './config/config';
+import { environments } from './environments';
+import config from './config';
 
 @Module({
   imports: [
     ProductsModule,
     ConfigModule.forRoot({
-      envFilePath:
-        environment[process.env.NODE_ENV] || './config/env/.development.env',
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+      }),
     }),
     DatabaseModule,
   ],
