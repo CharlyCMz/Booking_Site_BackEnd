57de415ece1611cb252c8758f1ba047e9cb49a11
Charly Correa <chuck.scrumdev@gmail.com>
2023-03-16 11:40:47 -0500

    First Entity definition for Category and its usage in the Product Module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesService } from './services/categories.service';
+import { Category } from './entities/category.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
